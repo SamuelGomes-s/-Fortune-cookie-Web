e132f482b68da8b9be9af1d4d3cd4a315aaf7681
samuelg <sam2018gs@gmail.com>
2025-01-26 16:40:37 -0300

    Criando logica de quebrar e recarregar biscoito

--- src/App.js
+++ src/App.js
@@ -9,17 +9,35 @@ import { MdOutlineWifiProtectedSetup } from "react-icons/md";
 import { GiBrokenBone } from "react-icons/gi";
 import { mensages } from "../src/assets/LuckyText/index"
 
-function App() {
+export default function App() {
+
   const [cookieIMG, setCookieIMG] = useState(Cookie);
+  const [msg, setMsg] = useState('')
+
   function handleBrokenCookie() {
+    if (cookieIMG === BrokenCookie) {
+      return alert("Pegue um novo biscoito...")
+    }
     setCookieIMG(BrokenCookie)
+    handleRandomMsg()
   }
   function handleReloadCookie() {
+    if (cookieIMG === Cookie) {
+      return
+    }
+    setMsg('')
     setCookieIMG(Cookie)
   }
+
+  function handleRandomMsg() {
+    let index = Math.floor(Math.random() * mensages.length)
+    setMsg(mensages[index])
+  }
+
   return (
     <Background>
       <ContainerApp>
+        {msg && (<Text>{msg}</Text>)}
         <Image
           onClick={handleBrokenCookie}
           src={cookieIMG}
@@ -29,17 +47,19 @@ function App() {
           <MdOutlineWifiProtectedSetup size={50} />
         </Button>
         <Button color={'rgba(255, 221, 41, 0.8)'} onClick={handleBrokenCookie}>
-          <GiBrokenBone size={50} />
+          <GiBrokenBone size={50} color="#fff" />
         </Button>
       </ContainerApp>
     </Background>
   );
 }
+
 const Background = styled.div`
   width: 100dvw;
   height: 100dvh;
   background-color: rgba(12,12,12,0.7);
 `;
 
-
-export default App;
+const Text = styled.h1`
+  color: #fff;
+`;
--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -3,16 +3,18 @@ import styled from "styled-components";
 const Container = styled.div`
     width: 700px;
     position: relative;
-    top:15%;
+    top: 15%;
     right: 0;
     left: 0;
-    margin:  0 auto;
+    margin: 0 auto;
     display: flex;
     align-items: center;
     flex-direction: column;
+
+    /* Quando a tela for menor ou igual a 700px */
+    @media screen and (max-width: 700px) {
+        width: 80%;
+    }
 `;
 
-
-export {
-    Container
-}
+export { Container };
